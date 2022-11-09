d54ac01d48f9ebc6141644ec7bee92aa82f1d779
nmcgregor15 <68406747+nmcgregor15@users.noreply.github.com>
2022-11-09 12:17:04 -0500

    finished studio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -12,17 +13,34 @@ function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
 
+  const [cart, setCart] = useState([]);
+  const [totalPrice, setPrice] = useState(0);
+
+  const updatePrice = () => {
+    let sum = 0; 
+    cart.forEach( i=> sum += i.price);
+    setPrice(sum);
+  }
+
+  useEffect( () => {
+    updatePrice();
+  }, [cart])
+
+
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        //<p>Bakery Item {index}</p> // replace with BakeryItem component
+        <BakeryItem item={item} cart={cart} updateCart={setCart}/>
       ))}
+
 
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        {cart.map(e => <p>{e.name} {e.price}</p>)}
+        <h3>Total Price = ${totalPrice}</h3>
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,22 @@
 // TODO: create a component that displays a single bakery item
+
+export default  function BakeryItem(props) {
+    const item = props.item;
+    const updateCart = props.updateCart;
+    const cart = props.cart;
+    const handleClick = () => {
+        updateCart([...cart, item]);
+
+    }
+
+    return (
+        <div>
+            <p> {item.name} ${item.price}  
+            <button onClick={handleClick}>Add to Cart</button>
+            </p>
+
+            <img src = {item.image}></img>
+        </div>
+
+    );
+}
